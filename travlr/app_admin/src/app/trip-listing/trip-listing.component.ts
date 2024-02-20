472b65879e788bb5f52daad200b198979e4771e5
PurePotato <catiebug99@hotmail.com>
2024-02-20 00:00:24 -0500

    Created Trip Service

--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
+//import { trips } from '../data/trips';
+import { Trip } from '../models/trips';
+import { TripDataService } from '../services/trip-data.service';
 
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
-  styleUrls: ['./trip-listing.component.css']
+  styleUrls: ['./trip-listing.component.css'],
+  providers: [TripDataService]
 })
 export class TripListingComponent implements OnInit {
-  trips: Array<any> = trips
-  constructor() { }
+  //trips: Array<any> = trips
+  trips: Trip[];
+  message: string;
+  constructor(private tripDataService: TripDataService) { }
 
-  ngOnInit() {
+  private getTrips(): void {
+    console.log('Inside TripListingComponent#getTrips');
+    this.message = 'Searching for trips';
+    this.tripDataService
+      .getTrips()
+      .then(foundTrips => {
+        this.message = foundTrips.length > 0 ? '' : 'No trips found';
+        this.trips = foundTrips;
+      });
+  }
+  ngOnInit(): void {
+    this.getTrips();
   }
 
 }
