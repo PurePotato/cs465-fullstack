736ed6e78057b84f185aa67a6a8e46704a692720
PurePotato <catiebug99@hotmail.com>
2024-02-27 12:43:06 -0500

    Module 7: Security pt1 redo

--- travlr/app.js
+++ travlr/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,6 +6,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs')
 require('./app_api/models/db');
+var passport = require('passport');
+require('./app_api/config/passport');
+
 
 var indexRouter = require("./app_server/routes/index");
 var travelRouter = require("./app_server/routes/travel");
@@ -21,7 +25,7 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
 // register handlebars partials(https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -29,11 +33,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 //allow CORS (cross origin resource sharing)
 app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
@@ -49,12 +54,12 @@ app.use("/contact", contactRouter);
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -62,6 +67,14 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({ "message": err.name + ": " + err.message })
+  }
 });
 
 module.exports = app;
--- travlr/app_api/models/db.js
+++ travlr/app_api/models/db.js
@@ -6,58 +6,67 @@ const readLine = require('readline');
 mongoose.set('useUnifiedTopology', true);
 
 const connect = () => {
-    setTimeout(
-        () => mongoose.connect(dbURI,{
-            useNewUrlParser: true, 
-            useCreateIndex: true
-        }), 1000);
+  setTimeout(
+    () => mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    }), 1000);
 }
 
 mongoose.connection.on('connected', () => {
-    console.log(`Mongoose connected to ${dbURI}`)
+  console.log(`Mongoose connected to ${dbURI}`)
 });
 
 mongoose.connection.on('error', err => {
-    console.log(`Mongoose connection error: ', err`)
+  console.log(`Mongoose connection error: ', err`)
 });
 
 mongoose.connection.on('disconnected', () => {
-    console.log(`Mongoose disconnected`)
+  console.log(`Mongoose disconnected`)
 });
 
-if (process.platform === 'win32'){
-    const rl = readLine.createInterface ({
-      input: process.stdin,
-      output: process.stdout
-    });
-    rl.on ('SIGINT', () => {
-      process.emit ("SIGINT");
-    });
-  }
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on('SIGINT', () => {
+    process.emit("SIGINT");
+  });
+}
 
-  const gracefulShutdown = (msg, callback) => {                1
-    mongoose.connection.close( () => {                         2
-      console.log(`Mongoose disconnected through ${msg}`);     3
-      callback();                                              3
-    });
-  };
+const gracefulShutdown = (msg, callback) => {
+  1
+  mongoose.connection.close(() => {
+    2
+    console.log(`Mongoose disconnected through ${msg}`); 3
+    callback(); 3
+  });
+};
 
-  process.once('SIGUSR2', () => {                        1
-    gracefulShutdown('nodemon restart', () => {          2
-      process.kill(process.pid, 'SIGUSR2');              2
-    });
+process.once('SIGUSR2', () => {
+  1
+  gracefulShutdown('nodemon restart', () => {
+    2
+    process.kill(process.pid, 'SIGUSR2'); 2
   });
-  process.on('SIGINT', () => {                           3
-    gracefulShutdown('app termination', () => {          4
-      process.exit(0);                                   4
-    });
+});
+process.on('SIGINT', () => {
+  3
+  gracefulShutdown('app termination', () => {
+    4
+    process.exit(0); 4
   });
-  process.on('SIGTERM', () => {                          5
-    gracefulShutdown('Heroku app shutdown', () => {      6
-      process.exit(0);                                   6
-    });
+});
+process.on('SIGTERM', () => {
+  5
+  gracefulShutdown('Heroku app shutdown', () => {
+    6
+    process.exit(0); 6
   });
+});
 
 connect();
 
-  require('./travlr');
+require('./travlr');
+require('./user');
