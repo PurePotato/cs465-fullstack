6bdea7ab98481387045860d7e638ae6129a77be7
PurePotato <catiebug99@hotmail.com>
2024-02-27 12:01:38 -0500

    Revert "Module 7: Security pt1"
    This reverts commit b029647fd5a19b74b7e48015cd2f331f2e370cc3.

--- travlr/app.js
+++ travlr/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs')
 require('./app_api/models/db');
-require('dotenv').config();
 
 var indexRouter = require("./app_server/routes/index");
 var travelRouter = require("./app_server/routes/travel");
@@ -16,15 +15,13 @@ var newsRouter = require("./app_server/routes/news");
 var aboutRouter = require("./app_server/routes/about");
 var contactRouter = require("./app_server/routes/contact");
 var apiRouter = require("./app_api/routes/index");
-var passport = require("passport");
-var app = express();
 
-require('./app_api/config/passport');
+var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
 // register handlebars partials(https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -36,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //allow CORS (cross origin resource sharing)
 app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
@@ -52,21 +49,12 @@ app.use("/contact", contactRouter);
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// catch unauthorized error and create 401
-app.use((req, res, next) => {
-  if (err.name === 'UnauthorizedError') {
-    res
-      .status(401)
-      .json({ "message": err.name + ": " + err.message })
-  }
-});
-
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const tripsController = require('../controllers/trips');
-const jwt = require('express-jwt');
-const auth = jwt({
-    secret: process.env.JWT_SECRET,
-    userProperty: 'payload'
-});
-const authController = require("../controllers/authentication");
 
+const tripsController = require('../controllers/trips');
 
 router
     .route('/trips')
     .get(tripsController.tripsList)
-    .post(auth, tripsController.tripsAddTrip);
-
+    .post(tripsController.tripsAddTrip);
+    
 router
     .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode)
-    .put(auth, tripsController.tripsUpdateTrip);
-
-router
-    .route('/login')
-    .post(authController.login);
-
-router
-    .route('/register')
-    .put(authController.register);
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
